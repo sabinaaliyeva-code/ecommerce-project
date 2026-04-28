8ef769e73b1b104efb7a5de47d1cede2d78b9cbf
Aliyeva Sabina <sabinaaliyeva-code>
2026-04-28 12:00:33 +0400

    fix project

--- JS/Services/api.js
+++ JS/Services/api.js
@@ -1,7 +1,7 @@
 
 
 export async function getProduct(){
-    let response = await fetch("./product.json");
+    let response = await fetch("/product.json");
     let data= await response.json();
     return data;
     
